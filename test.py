e9e793edcef982a27d755b9988c99bf31575ebd3
Nicky Harpor <nickyharpor@mail.ru>
2021-11-07 23:31:24 +0330

    added test file

---
+++ test.py
@@ -0,0 +1,24 @@
+from web3 import Web3
+from pprint import pprint
+
+from conf import Config
+
+def print_recent_blocks(num):
+    w3 = Web3(Web3.WebsocketProvider(Config.wss1))
+    last = w3.eth.block_number
+    first = last - num
+    num_iterator = first
+    while num_iterator <= last:
+        block_info = w3.eth.get_block(num_iterator, full_transactions=True)
+        pprint(w3.toJSON(block_info))
+        num_iterator += 1
+
+def print_blocks(first, last):
+    w3 = Web3(Web3.WebsocketProvider(Config.wss1))
+    num_iterator = first
+    while num_iterator <= last:
+        block_info = w3.eth.get_block(num_iterator, full_transactions=True)
+        pprint(w3.toJSON(block_info))
+        num_iterator += 1
+
+print_recent_blocks(100)
