7a86b63ee72162e4e8c7525c96b0ed8a2dc7c41d
Nicky Harpor <nickyharpor@mail.ru>
2021-11-07 23:32:20 +0330

    added config file

---
+++ conf.py
@@ -0,0 +1,6 @@
+class Config:
+
+    elastic_node = ''
+    wss1 = "ws://prod-forge-us-west-2-sentry-000-public.prod.findora.org:8546"
+    wss2 = "ws://prod-forge-us-west-2-sentry-001-public.prod.findora.org:8546"
+    rpc_url = "https://prod-forge.prod.findora.org:8545"
